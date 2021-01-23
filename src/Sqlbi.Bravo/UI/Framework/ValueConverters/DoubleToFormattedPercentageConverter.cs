adf06c449f281f61d590eea7cb38d9c1c7ffff35
Matt Lacey <matt@mrlacey.co.uk>
2021-01-23 11:45:24 +0000

    Show DB% to 1dp & "<0.1" for very small

--- src/Sqlbi.Bravo/UI/Framework/ValueConverters/DoubleToFormattedPercentageConverter.cs
+++ src/Sqlbi.Bravo/UI/Framework/ValueConverters/DoubleToFormattedPercentageConverter.cs
@@ -9,33 +9,9 @@ namespace Sqlbi.Bravo.UI.Framework.ValueConverters
         {
             if (value is double val)
             {
-                var str = (val * 100).ToString();
-
-                // This is a crude formatter for showing one significant figure
-                // For simplicity it rounds down.
-                if (str.StartsWith("0"))
-                {
-                    for (var i = 2; i < str.Length; i++)
-                    {
-                        if (str[i] != '0')
-                        {
-                            return $"{str.Substring(0, i + 1)} %";
-                        }
-                        else
-                        {
-                            if (i > 4)
-                            {
-                                break;
-                            }
-                        }
-                    }
-
-                    return "0.0000 %";
-                }
-                else
-                {
-                    return (val * 100).ToString("#0.#' %'");
-                }
+                return val < 0.001
+                    ? "<0.1 %"
+                    : (val * 100).ToString("#0.#' %'");
             }
 
             return string.Empty;
