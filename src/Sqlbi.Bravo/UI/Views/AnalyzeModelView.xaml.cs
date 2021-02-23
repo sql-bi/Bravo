d9e73256f28ce195a196a6175ccba222cd347d68
Matt Lacey <matt@mrlacey.co.uk>
2021-02-23 16:56:27 +0000

    Ensure the Tables TreeView is always displayed

--- src/Sqlbi.Bravo/UI/Views/AnalyzeModelView.xaml.cs
+++ src/Sqlbi.Bravo/UI/Views/AnalyzeModelView.xaml.cs
@@ -12,5 +12,20 @@ namespace Sqlbi.Bravo.UI.Views
             //// If don't do this we get a crash 'EditItem is not allowed' when a text entry is doubleclicked.
             e.Cancel = true;
         }
+
+        private void TablesTreeview_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if (e.OriginalSource is TreeView tv)
+            {
+                // There should always be items but sometimes they don't get displayed
+                if (!tv.HasItems)
+                {
+                    // Force the control to refresh
+                    // So they will be displayed
+                    tv.Items.Refresh();
+                    tv.UpdateLayout();
+                }
+            }
+        }
     }
 }
