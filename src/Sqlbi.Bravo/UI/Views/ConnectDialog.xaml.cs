f65c7a7ff7eef780eb0f2859890f507a8b873882
Matt Lacey <matt@mrlacey.co.uk>
2021-04-16 20:36:24 +0100

    tidy usings

--- src/Sqlbi.Bravo/UI/Views/ConnectDialog.xaml.cs
+++ src/Sqlbi.Bravo/UI/Views/ConnectDialog.xaml.cs
@@ -1,19 +1,9 @@
 ﻿using MahApps.Metro.Controls;
 using Sqlbi.Bravo.UI.ViewModels;
-using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace Sqlbi.Bravo.UI.Views
 {
