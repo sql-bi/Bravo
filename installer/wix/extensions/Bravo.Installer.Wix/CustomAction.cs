7f4f9716c4d7cb24b19aed87396a7ddd830c4393
Alberto Spelta <alberto.spelta@live.com>
2021-12-20 12:11:03 +0100

    Installer - update wix custom action

--- installer/wix/extensions/Bravo.Installer.Wix/CustomAction.cs
+++ installer/wix/extensions/Bravo.Installer.Wix/CustomAction.cs
@@ -2,13 +2,12 @@ using Microsoft.ApplicationInsights;
 using Microsoft.ApplicationInsights.DataContracts;
 using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.Deployment.WindowsInstaller;
-using Microsoft.Win32;
 using System;
 using System.IO;
 using System.Security.Cryptography;
 using System.Text;
 
-namespace Sqlbi.Bravo.Installer
+namespace Sqlbi.Bravo.Installer.Wix
 {
     public class CustomActions
     {
@@ -19,20 +18,20 @@ namespace Sqlbi.Bravo.Installer
         private const string ExternalToolsFolderPath = @"Microsoft Shared\Power BI Desktop\External Tools";
 
         [CustomAction]
-        public static ActionResult PowerBIDesktopRegisterExternalTool(Session session)
+        public static ActionResult PBIDesktopRegisterExternalTool(Session session)
         {
-            session.Log($"BRAVOLOG BEGIN ({ nameof(PowerBIDesktopRegisterExternalTool) })");
+            session.Log($"BRAVO[BEGIN] ({ nameof(PBIDesktopRegisterExternalTool) })");
             try
             {
                 foreach (var pairs in session.CustomActionData)
-                    session.Log($"BRAVOLOG LOG ({ nameof(PowerBIDesktopRegisterExternalTool) }) - CustomActionData({ pairs.Key }, { pairs.Value })");
+                    session.Log($"BRAVO[LOG] ({ nameof(PBIDesktopRegisterExternalTool) }) - CustomActionData({ pairs.Key }, { pairs.Value })");
 
                 var installedExecutablePath = session.CustomActionData[CustomActionDataInstalledExecutablePath];
                 installedExecutablePath = installedExecutablePath.Replace("\\", "\\\\");
 
                 var commonProgramPath = Environment.GetFolderPath(Environment.SpecialFolder.CommonProgramFilesX86);
                 var path = Path.Combine(commonProgramPath, ExternalToolsFolderPath, ExternalToolsPbiToolFileName);
-                session.Log($"BRAVOLOG LOG ({ nameof(PowerBIDesktopRegisterExternalTool) }) - pbitool.json ({ path })");
+                session.Log($"BRAVO[LOG] ({ nameof(PBIDesktopRegisterExternalTool) }) - pbitool.json ({ path })");
 
                 var content = File.ReadAllText(path);
                 content = content.Replace("<#!PATH!#>", installedExecutablePath);
@@ -40,29 +39,29 @@ namespace Sqlbi.Bravo.Installer
             }
             catch (Exception ex)
             {
-                session.Log($"BRAVOLOG ERROR ({ nameof(PowerBIDesktopRegisterExternalTool) }) - { ex }");
+                session.Log($"BRAVO[ERROR] ({ nameof(PBIDesktopRegisterExternalTool) }) - { ex }");
 
                 var telemetryClient = GetTelemetryClient(session);
                 telemetryClient.TrackException(ex);
                 telemetryClient.Flush();
             }
 
-            session.Log($"BRAVOLOG END ({ nameof(PowerBIDesktopRegisterExternalTool) })");
+            session.Log($"BRAVO[END] ({ nameof(PBIDesktopRegisterExternalTool) })");
             return ActionResult.Success;
         }
 
         [CustomAction]
         public static ActionResult AfterInstall(Session session)
         {
-            session.Log($"BRAVOLOG BEGIN ({ nameof(AfterInstall) })");
+            session.Log($"BRAVO[BEGIN] ({ nameof(AfterInstall) })");
             try
             {
                 foreach (var pairs in session.CustomActionData)
-                    session.Log($"BRAVOLOG LOG ({ nameof(AfterInstall) }) - CustomActionData({ pairs.Key }, { pairs.Value })");
+                    session.Log($"BRAVO[LOG] ({ nameof(AfterInstall) }) - CustomActionData({ pairs.Key }, { pairs.Value })");
 
                 if (IsTelemetryEnabled(session))
                 {
-                    session.Log($"BRAVOLOG LOG ({ nameof(AfterInstall) }) - TrackEvent");
+                    session.Log($"BRAVO[LOG] ({ nameof(AfterInstall) }) - TrackEvent");
                     var telemetryEvent = new EventTelemetry(name: "Install");
                     var telemetryClient = GetTelemetryClient(session);
                     telemetryClient.TrackEvent(telemetryEvent);
@@ -71,25 +70,25 @@ namespace Sqlbi.Bravo.Installer
             }
             catch (Exception ex)
             {
-                session.Log($"BRAVOLOG ERROR ({ nameof(AfterInstall) }) { ex }");
+                session.Log($"BRAVO[ERROR] ({ nameof(AfterInstall) }) { ex }");
             }
 
-            session.Log($"BRAVOLOG END ({ nameof(AfterInstall) })");
+            session.Log($"BRAVO[END] ({ nameof(AfterInstall) })");
             return ActionResult.Success;
         }
 
         [CustomAction]
         public static ActionResult AfterUninstall(Session session)
         {
-            session.Log($"BRAVOLOG BEGIN ({ nameof(AfterUninstall) })");
+            session.Log($"BRAVO[BEGIN] ({ nameof(AfterUninstall) })");
             try
             {
                 foreach (var pairs in session.CustomActionData)
-                    session.Log($"BRAVOLOG LOG ({ nameof(AfterUninstall) }) - CustomActionData({ pairs.Key }, { pairs.Value })");
+                    session.Log($"BRAVO[LOG] ({ nameof(AfterUninstall) }) - CustomActionData({ pairs.Key }, { pairs.Value })");
 
                 if (IsTelemetryEnabled(session))
-                { 
-                    session.Log($"BRAVOLOG LOG ({ nameof(AfterUninstall) }) - TrackEvent");
+                {
+                    session.Log($"BRAVO[LOG] ({ nameof(AfterUninstall) }) - TrackEvent");
                     var telemetryClient = GetTelemetryClient(session);
                     var telemetryEvent = new EventTelemetry(name: "Uninstall");
                     telemetryClient.TrackEvent(telemetryEvent);
@@ -98,10 +97,10 @@ namespace Sqlbi.Bravo.Installer
             }
             catch (Exception ex)
             {
-                session.Log($"BRAVOLOG ERROR ({ nameof(AfterUninstall) }) { ex }");
+                session.Log($"BRAVO[ERROR] ({ nameof(AfterUninstall) }) { ex }");
             }
 
-            session.Log($"BRAVOLOG END ({ nameof(AfterUninstall) })");
+            session.Log($"BRAVO[END] ({ nameof(AfterUninstall) })");
             return ActionResult.Success;
         }
 
@@ -112,7 +111,7 @@ namespace Sqlbi.Bravo.Installer
             var telemetryConfiguration = TelemetryConfiguration.CreateDefault();
             telemetryConfiguration.InstrumentationKey = "47a8970c-6293-408a-9cce-5b7b311574d3";
             telemetryConfiguration.DisableTelemetry = false;
-            
+
             var telemetryClient = new TelemetryClient(telemetryConfiguration);
             telemetryClient.Context.Device.OperatingSystem = Environment.OSVersion.ToString();
             telemetryClient.Context.Component.Version = productVersion;
--- installer/wix/extensions/Bravo.Installer.Wix/Properties/AssemblyInfo.cs
+++ installer/wix/extensions/Bravo.Installer.Wix/Properties/AssemblyInfo.cs
@@ -1,11 +1,10 @@
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Sqlbi.Bravo.Installer")]
+[assembly: AssemblyTitle("Sqlbi.Bravo.Installer.Wix")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Sqlbi.Bravo.Installer")]
