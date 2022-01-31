9c3d0e0292c91ef87d932e78296fd01b2d8b36c8
Alberto Spelta <alberto.spelta@live.com>
2022-01-31 16:49:24 +0100

    Removed HandleHotKeys

--- src/Infrastructure/AppWindow.cs
+++ src/Infrastructure/AppWindow.cs
@@ -126,9 +126,6 @@
 
         private void OnWindowCreated(object? sender, EventArgs e)
         {
-#if !DEBUG
-            HandleHotKeys(register: true);
-#endif
             _windowSubclass = AppWindowSubclass.Hook(_window);
 
             // Every time a Photino application starts up, Photino.Native attempts to creates a shortcut in Windows start menu.
@@ -146,59 +143,8 @@
         private bool OnWindowClosing(object sender, EventArgs e)
         {
             NotificationHelper.ClearNotifications();
-#if !DEBUG
-            HandleHotKeys(register: false);
-#endif
+
             return false; // Returning true stops window from closing
-        }
-
-        /// <summary>
-        /// Register hotkeys in order to override WebView2 accelerator keys like Ctrl-P for print, Ctrl-R and F5 for reload ecc..
-        /// </summary>
-        private void HandleHotKeys(bool register)
-        {
-            // TODO: instead of register hot-keys we should use property 'Microsoft.Web.WebView2.Core.CoreWebView2Settings.AreBrowserAcceleratorKeysEnabled'
-            // Unfortunatly CoreWebView2Settings.AreBrowserAcceleratorKeysEnabled property is not yet implemented on Photino.NET
-
-            const int HOTKEY_CONTROL_F = 1;       // Ctrl-F and F3 for Find on Page
-            const int HOTKEY_F3 = 2;              // Ctrl-F and F3 for Find on Page
-            const int HOTKEY_CONTROL_P = 3;       // Ctrl-P for Print
-            const int HOTKEY_CONTROL_R = 4;       // Ctrl-R and F5 for Reload
-            const int HOTKEY_F5 = 5;              // Ctrl-R and F5 for Reload
-            //const int HOTKEY_CONTROL_PLUS = 6;  // Ctrl-Plus and Ctrl-Minus for zooming
-            //const int HOTKEY_CONTROL_MINUS = 7; // Ctrl-Plus and Ctrl-Minus for zooming
-            const int HOTKEY_CONTROL_S = 8;       // Ctrl-S for SaveAs
-            const int HOTKEY_ALT_LEFTARROW = 9;   // Alt-Left arrow for Back
-            const int HOTKEY_ALT_RIGHTARROW = 10; // Alt-Right arrow for Forward
-
-            var hWnd = _window.WindowHandle;
-
-            if (register)
-            {
-                _ = User32.RegisterHotKey(hWnd, id: HOTKEY_CONTROL_F, User32.MODEKEY.MOD_CONTROL, System.Windows.Forms.Keys.F);
-                _ = User32.RegisterHotKey(hWnd, id: HOTKEY_F3, User32.MODEKEY.MOD_NONE, System.Windows.Forms.Keys.F3);
-                _ = User32.RegisterHotKey(hWnd, id: HOTKEY_CONTROL_P, User32.MODEKEY.MOD_CONTROL, System.Windows.Forms.Keys.P);
-                _ = User32.RegisterHotKey(hWnd, id: HOTKEY_CONTROL_R, User32.MODEKEY.MOD_CONTROL, System.Windows.Forms.Keys.R);
-                _ = User32.RegisterHotKey(hWnd, id: HOTKEY_F5, User32.MODEKEY.MOD_NONE, System.Windows.Forms.Keys.F5);
-                //_ = User32.RegisterHotKey(hWnd, id: HOTKEY_CONTROL_PLUS, User32.KeyModifier.MOD_CONTROL, System.Windows.Forms.Keys.Add);
-                //_ = User32.RegisterHotKey(hWnd, id: HOTKEY_CONTROL_MINUS, User32.KeyModifier.MOD_CONTROL, System.Windows.Forms.Keys.Subtract);
-                _ = User32.RegisterHotKey(hWnd, id: HOTKEY_CONTROL_S, User32.MODEKEY.MOD_CONTROL, System.Windows.Forms.Keys.S);
-                _ = User32.RegisterHotKey(hWnd, id: HOTKEY_ALT_LEFTARROW, User32.MODEKEY.MOD_ALT, System.Windows.Forms.Keys.Left);
-                _ = User32.RegisterHotKey(hWnd, id: HOTKEY_ALT_RIGHTARROW, User32.MODEKEY.MOD_ALT, System.Windows.Forms.Keys.Right);
-            }
-            else
-            {
-                _ = User32.UnregisterHotKey(hWnd, id: HOTKEY_CONTROL_F);
-                _ = User32.UnregisterHotKey(hWnd, id: HOTKEY_F3);
-                _ = User32.UnregisterHotKey(hWnd, id: HOTKEY_CONTROL_P);
-                _ = User32.UnregisterHotKey(hWnd, id: HOTKEY_CONTROL_R);
-                _ = User32.UnregisterHotKey(hWnd, id: HOTKEY_F5);
-                //_ = User32.UnregisterHotKey(hWnd, id: HOTKEY_CONTROL_PLUS);
-                //_ = User32.UnregisterHotKey(hWnd, id: HOTKEY_CONTROL_MINUS);
-                _ = User32.UnregisterHotKey(hWnd, id: HOTKEY_CONTROL_S);
-                _ = User32.UnregisterHotKey(hWnd, id: HOTKEY_ALT_LEFTARROW);
-                _ = User32.UnregisterHotKey(hWnd, id: HOTKEY_ALT_RIGHTARROW);
-            }
         }
 
         /// <summary>
