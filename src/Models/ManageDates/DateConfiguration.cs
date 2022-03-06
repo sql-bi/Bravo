e6df067463ac180a71825d71692b74366ce0e391
Alberto Spelta <alberto.spelta@live.com>
2022-03-06 19:22:33 +0100

    HACK  to fix TemplateException 'Holidays table 'Holidays' not found.'

--- src/Models/ManageDates/DateConfiguration.cs
+++ src/Models/ManageDates/DateConfiguration.cs
@@ -271,6 +271,11 @@
                     else
                     {
                         templateConfiguration.Templates = templateConfiguration.Templates!.Except(new[] { holidaysTemplateEntry, holidaysDefinitionTemplateEntry }).ToArray();
+                        
+                        // HACK >> to fix TemplateException($"Holidays table '{config.HolidaysReference?.TableName}' not found.");
+                        templateConfiguration.HolidaysReference = null;
+                        templateConfiguration.HolidaysDefinitionTable = null;
+                        // HACK <<
                     }
                 }
             }
