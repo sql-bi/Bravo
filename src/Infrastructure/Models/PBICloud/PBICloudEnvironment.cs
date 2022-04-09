806c930470437220961586d1c8c00f616ddbafd2
Alberto Spelta <albertospelta@users.noreply.github.com>
2022-04-09 17:18:34 +0200

    Refactoring PBICloudEnvironment

---
+++ src/Infrastructure/Models/PBICloud/PBICloudEnvironment.cs
@@ -0,0 +1,53 @@
+﻿namespace Sqlbi.Bravo.Infrastructure.Models.PBICloud
+{
+    using System.Diagnostics;
+
+    [DebuggerDisplay("{Name}, {AuthorityUri}")]
+    internal class PBICloudEnvironment: IPBICloudEnvironment
+    {
+        public PBICloudEnvironmentType? Type { get; set; }
+
+        public string? AzureADAuthority { get; set; }
+
+        public string? AzureADClientId { get; set; }
+
+        public string? AzureADRedirectAddress { get; set; }
+
+        public string? AzureADResource { get; set; }
+
+        public string? GlobalServiceEndpoint { get; set; }
+    }
+
+    public interface IPBICloudEnvironment
+    {
+        /// <summary>
+        /// Type of the PowerBI environment.
+        /// </summary>
+        PBICloudEnvironmentType? Type { get; set; }
+
+        /// <summary>
+        /// Azure Active Directory Secure Token Service (STS) Authority
+        /// </summary>
+        string? AzureADAuthority { get; set; }
+
+        /// <summary>
+        /// Azure Active Directory (AAD) ClientId for the AAD application
+        /// </summary>
+        string? AzureADClientId { get; set; }
+
+        /// <summary>
+        /// Azure Active Directory (AAD) Redirect Address for AAD application
+        /// </summary>
+        string? AzureADRedirectAddress { get; set; }
+
+        /// <summary>
+        /// Azure Active Directory Resource to authenticate against
+        /// </summary>
+        string? AzureADResource { get; set; }
+
+        /// <summary>
+        /// Endpoint to communicate with the PowerBI global service
+        /// </summary>
+        string? GlobalServiceEndpoint { get; set; }
+    }
+}
---
+++ src/Infrastructure/Models/PBICloud/PBICloudEnvironmentType.cs
@@ -0,0 +1,60 @@
+﻿namespace Sqlbi.Bravo.Infrastructure.Models.PBICloud
+{
+    public enum PBICloudEnvironmentType
+    {
+        /// <summary>
+        /// GlobalService "cloudName": "GlobalCloud",
+        /// </summary>
+        Public = 0,
+
+        /// <summary>
+        /// GlobalService "cloudName": "GermanyCloud"
+        /// </summary>
+        Germany = 1,
+
+        /// <summary>
+        /// GlobalService "cloudName": "USGovCloud" - (US Government Community Cloud)
+        /// </summary>
+        USGov = 2,
+
+        /// <summary>
+        /// GlobalService "cloudName": "ChinaCloud",
+        /// </summary>
+        China = 3,
+
+        /// <summary>
+        /// GlobalService "cloudName": "USGovDoDL4Cloud" - (US Government Community Cloud High)
+        /// </summary>
+        USGovHigh = 4,
+
+        /// <summary>
+        /// GlobalService "cloudName": "USGovDoDL5Cloud" - (US Department of Defense)
+        /// </summary>
+        USGovMil = 5,
+
+        /// <summary>
+        /// ???
+        /// </summary>
+        Custom = 6
+    }
+
+    internal static class PBICloudEnvironmentTypeExtensions
+    {
+        public static string ToGlobalServiceCloudName(this PBICloudEnvironmentType environmentType)
+        {
+            var cloudName = environmentType switch
+            {
+                PBICloudEnvironmentType.Public => "GlobalCloud",
+                PBICloudEnvironmentType.Germany => "GermanyCloud",
+                PBICloudEnvironmentType.USGov => "USGovCloud",
+                PBICloudEnvironmentType.China => "ChinaCloud",
+                PBICloudEnvironmentType.USGovHigh => "USGovDoDL4Cloud",
+                PBICloudEnvironmentType.USGovMil => "USGovDoDL5Cloud",
+                PBICloudEnvironmentType.Custom => throw new System.NotImplementedException(),
+                _ => throw new BravoUnexpectedInvalidOperationException($"Unhandled { nameof(PBICloudEnvironmentType) } value ({ environmentType })"),
+            };
+
+            return cloudName;
+        }
+    }
+}
