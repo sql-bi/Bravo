8910d9f7d4d1e32e43bdba81c92b5e5cb04b8189
Alberto Spelta <albertospelta@users.noreply.github.com>
2022-04-18 12:07:29 +0200

    Added Greek translation

--- src/Scripts/model/i18n/locales.ts
+++ src/Scripts/model/i18n/locales.ts
@@ -8,6 +8,7 @@ import cz from './cz';
 import da from './da';
 import de from './de';
 import fr from './fr';
+import gr from './gr';
 import it from './it';
 import nl from './nl';
 import pl from './pl';
@@ -19,6 +20,7 @@ const locales = {
     [da.locale]: da,
     [de.locale]: de,
     [fr.locale]: fr,
+    [gr.locale]: gr,
     [it.locale]: it,
     [nl.locale]: nl,
     [pl.locale]: pl,
