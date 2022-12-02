7f25b7e0fe45cb877b7abce6e889029a6fde39a1
Alberto Spelta <albertospelta@users.noreply.github.com>
2022-12-02 18:13:33 +0100

    Add Persian to locale.ts
    Added as requested from code review in #531

--- src/Scripts/model/i18n/locales.ts
+++ src/Scripts/model/i18n/locales.ts
@@ -8,6 +8,7 @@ import cz from './cz';
 import da from './da';
 import de from './de';
 import es from './es';
+import fa from './fa';
 import fr from './fr';
 import gr from './gr';
 import it from './it';
@@ -24,6 +25,7 @@ const locales = {
     [da.locale]: da,
     [de.locale]: de,
     [es.locale]: es,
+    [fa.locale]: fa,
     [fr.locale]: fr,
     [gr.locale]: gr,
     [it.locale]: it,
